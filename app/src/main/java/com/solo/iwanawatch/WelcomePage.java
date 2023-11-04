b1053dd91612f0ca0c21fa3d09b6afe267e1f4d6
Arif <a.v.rustamov@gmail.com>
2023-11-04 15:37:32 +0300

    + Main Activity + Welcome Message + Fonts

--- app/src/main/java/com/solo/iwanawatch/WelcomePage.java
+++ app/src/main/java/com/solo/iwanawatch/WelcomePage.java
@@ -2,7 +2,9 @@ package com.solo.iwanawatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomePage extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class WelcomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
